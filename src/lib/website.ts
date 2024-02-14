f01916d4e231b7e9271cc7dca969c38dd2205862
zhangxh1023 <zhangxh1023@gmail.com>
2024-02-14 15:59:00 +0800

    36kr douban topic website

--- src/lib/website.ts
+++ src/lib/website.ts
@@ -52,7 +52,27 @@ export enum Website {
   /**
    * 36kr快讯热点
    */
-  kr36_newsflashes = "kr36_newsflashes",
+  kr36_newsflashes_hot = "kr36_newsflashes_hot",
+
+  /**
+   * 36kr快讯股市
+   */
+  kr36_newsflashes_stock = "kr36_newsflashes_stock",
+
+  /**
+   * 36kr快讯公司
+   */
+  kr36_newsflashes_corp = "kr36_newsflashes_corp",
+
+  /**
+   * 36kr快讯宏观
+   */
+  kr36_newsflashes_macro = "kr36_newsflashes_macro",
+
+  /**
+   * 豆瓣话题趋势
+   */
+  douban_topic = "douban_topic",
 }
 
 export const getWebsiteName = (website: Website): string => {
@@ -66,7 +86,11 @@ export const getWebsiteName = (website: Website): string => {
   if (website === Website.hupu_soccer) return "虎扑足球";
   if (website === Website.hupu_bbs) return "虎扑步行街";
   if (website === Website.qq_qa) return "腾讯热门问答";
-  if (website === Website.kr36_newsflashes) return "36kr快讯热点";
+  if (website === Website.kr36_newsflashes_hot) return "36kr快讯热点";
+  if (website === Website.kr36_newsflashes_stock) return "36kr快讯股市";
+  if (website === Website.kr36_newsflashes_corp) return "36kr快讯公司";
+  if (website === Website.kr36_newsflashes_macro) return "36kr快讯宏观";
+  if (website === Website.douban_topic) return "豆瓣话题趋势";
   return "";
 };
 
@@ -89,8 +113,16 @@ export const getUriBySymbol = (website: Website, symbol: string): string => {
     return symbol;
   } else if (website === Website.qq_qa) {
     return `https://new.qq.com/rain/a/${symbol}`;
-  } else if (website === Website.kr36_newsflashes) {
+  } else if (website === Website.kr36_newsflashes_hot) {
     return `https://36kr.com${symbol}`;
+  } else if (website === Website.kr36_newsflashes_stock) {
+    return `https://36kr.com${symbol}`;
+  } else if (website === Website.kr36_newsflashes_corp) {
+    return `https://36kr.com${symbol}`;
+  } else if (website === Website.kr36_newsflashes_macro) {
+    return `https://36kr.com${symbol}`;
+  } else if (website === Website.douban_topic) {
+    return symbol;
   }
 
   return "";
